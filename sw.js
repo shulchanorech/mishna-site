e0cff9857d3f42fb4a1ee9edff62c7ae0dc348a5
shulchanorech <166443918+shulchanorech@users.noreply.github.com>
2026-09-22 00:23:52 +0300

    גרסה 38 — קפיצה יציבה לקישור ישיר

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mishna-v37';
+const CACHE='mishna-v38';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
